adba484abc78cd0be85331637c1f7f8b13028d77
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:18:14 +0800

    Update outputs.tf

--- modules/foundation/ram/outputs.tf
+++ modules/foundation/ram/outputs.tf
@@ -1,9 +1,1 @@
-output "ram_role_name" {
-  description = "Landing Zone ECS RAM角色名"
-  value       = alicloud_ram_role.lz_ecs_role.name
-}
-
-output "ram_role_arn" {
-  description = "Landing Zone ECS RAM角色ARN"
-  value       = alicloud_ram_role.lz_ecs_role.arn
-}
+output "role_name" { value = alicloud_ram_role.this.role_name }
