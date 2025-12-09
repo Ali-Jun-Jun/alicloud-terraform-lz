1730850fcf1f4246e6b2a4457a1931d610f26977
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:20:05 +0800

    Create main.tf

---
+++ environments/dev/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+    }
+  }
+}
+
+provider "alicloud" {
+  region = "cn-beijing"
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  environment = "dev"
+  vpc_cidr    = "10.1.0.0/16"
+  subnet_cidr = "10.1.1.0/24"
+  zone        = "cn-beijing-a"
+}
+
+module "ram_role" {
+  source = "../../modules/ram-role"
+  environment = "dev"
+}
+
+module "ecs" {
+  source = "../../modules/ecs"
+
+  environment         = "dev"
+  image_id            = "ubuntu_20_04_x64_20G_alibase_20230717.vhd"
+  instance_type       = "ecs.t5-lc1m2.small"
+  security_group_id   = module.vpc.security_group_id
+  vswitch_id          = module.vpc.vswitch_id
+  key_name            = "default"  # ← 替换为你在阿里云创建的密钥对名称
+}
