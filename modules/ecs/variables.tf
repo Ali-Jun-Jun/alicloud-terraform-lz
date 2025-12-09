e3a8b0b602d1f0e2fc3866c44ee95daf657826f1
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:35:00 +0800

    Add variables for ECS module configuration

---
+++ modules/ecs/variables.tf
@@ -0,0 +1,6 @@
+variable "environment"         { type = string }
+variable "image_id"            { type = string }
+variable "instance_type"       { type = string }
+variable "security_group_id"   { type = string }
+variable "vswitch_id"          { type = string }
+variable "key_name"            { type = string }
