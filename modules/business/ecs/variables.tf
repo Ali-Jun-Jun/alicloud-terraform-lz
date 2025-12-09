543d1bb872c3ccfbd4c3af8379c9e407d2103ccb
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:31:54 +0800

    Delete modules/business directory

--- modules/business/ecs/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "environment"         { type = string }
-variable "image_id"            { type = string }
-variable "instance_type"       { type = string }
-variable "security_group_id"   { type = string }
-variable "vswitch_id"          { type = string }
-variable "key_name"            { type = string }
