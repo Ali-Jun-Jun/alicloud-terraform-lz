50b315e322cf0f178e09c4a878f25f88c93fbd92
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:21:40 +0800

    Update Terraform organization name in backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "your-org-name"  # ← 替换成你的 HCP Terraform 组织名
+    organization = "test-aliyun"  # ← 替换成你的 HCP Terraform 组织名
 
     workspaces {
       name = "landingzone-dev"
