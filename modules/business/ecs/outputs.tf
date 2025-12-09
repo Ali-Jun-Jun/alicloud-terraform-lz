660824a6f7c38db4ab40f53de23bf7b5ececb058
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:16:32 +0800

    Update outputs.tf

--- modules/business/ecs/outputs.tf
+++ modules/business/ecs/outputs.tf
@@ -1,19 +1,3 @@
-output "ecs_instance_ids" {
-  description = "ECS实例ID列表"
-  value       = alicloud_ecs_instance.lz_ecs.*.id
-}
-
-output "ecs_public_ips" {
-  description = "ECS公网IP列表（仅测试环境）"
-  value       = alicloud_ecs_instance.lz_ecs.*.public_ip_address
-}
-
-output "ecs_private_ips" {
-  description = "ECS私有IP列表"
-  value       = alicloud_ecs_instance.lz_ecs.*.private_ip_address
-}
-
-output "security_group_id" {
-  description = "ECS安全组ID"
-  value       = alicloud_security_group.lz_ecs_sg.id
-}
+output "instance_id"  { value = alicloud_instance.this.id }
+output "public_ip"   { value = alicloud_instance.this.public_ip }
+output "private_ip"  { value = alicloud_instance.this.private_ip }
