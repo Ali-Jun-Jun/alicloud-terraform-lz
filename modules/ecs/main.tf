d8b38f08ce946ebf08f93076ca0a4905c3117651
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:34:39 +0800

    Create main.tf

---
+++ modules/ecs/main.tf
@@ -0,0 +1,16 @@
+resource "alicloud_instance" "this" {
+  instance_name             = "${var.environment}-web-server"
+  image_id                  = var.image_id
+  instance_type             = var.instance_type
+  security_groups           = [var.security_group_id]
+  vswitch_id                = var.vswitch_id
+  internet_max_bandwidth_out = 5
+  system_disk_category      = "cloud_efficiency"
+  system_disk_size          = 40
+  key_name                  = var.key_name
+  tags = {
+    Project     = "LandingZone"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
