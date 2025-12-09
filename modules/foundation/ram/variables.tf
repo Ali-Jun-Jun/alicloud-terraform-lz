79e6575cdae6653faa25c0bd04414922897dd2f8
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:17:57 +0800

    Update variables.tf

--- modules/foundation/ram/variables.tf
+++ modules/foundation/ram/variables.tf
@@ -1,14 +1,1 @@
-variable "env" {
-  type        = string
-  description = "部署环境（dev/test/prod）"
-  validation {
-    condition     = contains(["dev", "test", "prod"], var.env)
-    error_message = "环境必须为dev/test/prod之一"
-  }
-}
-
-variable "business_unit" {
-  type        = string
-  description = "业务单元标识（如retail/finance）"
-  default     = "retail"
-}
+variable "environment" { type = string }
