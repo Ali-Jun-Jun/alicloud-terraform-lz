05271f4b91b5376d193d58d2a57353b243ba3e0a
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 17:56:23 +0800

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -29,7 +29,7 @@ module "ecs" {
 
   environment         = "dev"
   image_id            = "ubuntu_22_04_x64_20G_alibase_20251103.vhd"
-  instance_type       = "ecs.t6-c4m1.large"
+  instance_type       = "ecs.e-c1m2.large"
   security_group_id   = module.vpc.security_group_id
   vswitch_id          = module.vpc.vswitch_id
   key_name            = "llj"  # ← 替换为你在阿里云创建的密钥对名称
