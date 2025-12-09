dacf383ab2f9cbbfad9263acb88781a12ce011bf
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:28:00 +0800

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -16,7 +16,7 @@ module "vpc" {
   environment = "dev"
   vpc_cidr    = "10.1.0.0/16"
   subnet_cidr = "10.1.1.0/24"
-  zone        = "cn-beijing-a"
+  zone        = "cn-beijing-e"
 }
 
 module "ram_role" {
@@ -28,9 +28,9 @@ module "ecs" {
   source = "../../modules/ecs"
 
   environment         = "dev"
-  image_id            = "ubuntu_20_04_x64_20G_alibase_20230717.vhd"
-  instance_type       = "ecs.t5-lc1m2.small"
+  image_id            = "ubuntu_22_04_x64_20G_alibase_20251103.vhd"
+  instance_type       = "ecs.t6-c4m1.large"
   security_group_id   = module.vpc.security_group_id
   vswitch_id          = module.vpc.vswitch_id
-  key_name            = "default"  # ← 替换为你在阿里云创建的密钥对名称
+  key_name            = "llj"  # ← 替换为你在阿里云创建的密钥对名称
 }
