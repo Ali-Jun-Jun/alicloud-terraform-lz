9ab519c98d815863d6f46ce7892649b3c8ec4310
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:35:15 +0800

    Add outputs for instance ID and IP addresses

---
+++ modules/ecs/outputs.tf
@@ -0,0 +1,3 @@
+output "instance_id" { value = alicloud_instance.this.id }
+output "public_ip"  { value = alicloud_instance.this.public_ip }
+output "private_ip" { value = alicloud_instance.this.private_ip }
