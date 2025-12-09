174bd10eea527be1cc0cb92815462bb78cf6458c
Ali-Jun-Jun <2668973019@qq.com>
2025-12-09 14:34:19 +0800

    Add outputs for VPC, VSwitch, and Security Group IDs

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,3 @@
+output "vpc_id"            { value = alicloud_vpc.this.id }
+output "vswitch_id"        { value = alicloud_vswitch.this.id }
+output "security_group_id" { value = alicloud_security_group.this.id }
